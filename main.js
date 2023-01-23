c7cac12e4985481257c77585c0104f0e840f1b03
Vorleak Yek <vorleakyek@gmail.com>
2023-01-23 06:57:46 +0000

    complet meme gallery project

---
+++ main.js
@@ -0,0 +1,24 @@
+var imageUrls = [];
+var $imageUrlInput = document.querySelector('input');
+var $addImageButton = document.querySelector('button');
+var $gallery = document.querySelector('main');
+
+$addImageButton.addEventListener('click', function () {
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (var i = 0; i < imageUrls.length; i++) {
+    var $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
